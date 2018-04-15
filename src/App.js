33f9997e612237a48d78e9fcb9f2c0dca9ce9b88
Camila Onofri <onofricamila2@gmail.com>
2018-04-15 20:42:30 -0300

    App.js modified

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        
           <h1> Camila's first React app :) </h1>
-     
       </div>
     );
+ // above code gets compiled to below code
+ // return React.createElement('div',{ className:"App"}, React.createElement('h1', null, 'Camila\'s first React app :)'));
   }
 }
 
